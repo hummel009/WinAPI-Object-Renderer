2658f8eae77eae3307728a1dc2595ce9d1763d10
Hummel009 <hummel.turbamentum@gmail.com>
2026-03-29 15:24:15 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,15 +6,6 @@ pluginManagement {
 	}
 }
 
-dependencyResolutionManagement {
-	repositoriesMode.set(RepositoriesMode.PREFER_PROJECT)
-	repositories {
-		mavenLocal()
-		mavenCentral()
-		gradlePluginPortal()
-	}
-}
-
 plugins {
 	id("org.gradle.toolchains.foojay-resolver-convention") version "latest.release"
 }
